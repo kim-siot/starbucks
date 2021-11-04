661c15c23401a9db1a97eb6fedbf739b47882c20
kim-siot <qiooiu@naver.com>
2021-11-04 23:39:56 +0900

    starubucks login

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
-//검색창
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function (){
-  searchInputEl.focus();
-});
-searchInputEl.addEventListener('focus', function (){
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder','통합검색');
-});
-searchInputEl.addEventListener('blur', function (){
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder','');
-});
 
 
 //배지 스크롤
@@ -144,9 +129,6 @@ floatingObject('.floating2', .5, 15);
 floatingObject('.floating3', 1.5, 20);
 
 
-// 2021
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
 
 
 const spyEls = document.querySelectorAll('section.scroll-spy');
